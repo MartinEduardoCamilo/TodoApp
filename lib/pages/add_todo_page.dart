09fb361fe607bc574d709b173d1decbc2b0d37e0
Martín Eduardo Camilo de la Cruz <54560181+MartinEduardoCamilo@users.noreply.github.com>
2022-11-02 22:04:42 -0400

    Vista del formulario

--- lib/pages/add_todo_page.dart
+++ lib/pages/add_todo_page.dart
@@ -31,21 +31,61 @@ class _AddTodoPageState extends State<AddTodoPage> {
               child: Container(
                 color: selectColor,
                 child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
-                        margin: const EdgeInsets.all(10), 
-                        child: TextFormField()
-                    ),
-                    const SizedBox(height: 20,),
-                    Container(
-                        margin: const EdgeInsets.all(10), 
+                        margin: const EdgeInsets.all(10),
                         child: TextFormField(
-                        )
-                    )
+                          controller: titulo,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                          decoration: const InputDecoration(
+                              hintText: "Titulo", border: InputBorder.none),
+                        )),
+                    Container(
+                        margin: const EdgeInsets.all(10),
+                        child: TextFormField(
+                          controller: asunto,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                          decoration: const InputDecoration(
+                              hintText: "Asunto", border: InputBorder.none),
+                        ))
                   ],
                 ),
               ),
-            )
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
+              child: Text("Seleccionar un color",style: TextStyle(fontSize: 20),),
+            ),
+            Expanded(
+                child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: Colors.primaries.length,
+              itemBuilder: (context, index) {
+                final color = Colors.primaries[index];
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
+                  child: InkWell(
+                    onTap: () => setState(() {
+                      selectColor = color;
+                    }),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: color,
+                    ),
+                  ),
+                );
+              },
+            ))
           ],
         ),
       ),
